2228dd3493db27552d4743485e422a977de816af
Puspita Vabna <sharif7761@gmail.com>
2021-08-14 22:48:24 +0600

    migration and models

---
+++ database/migrations/2021_08_14_162719_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('subtitle');
+            $table->string('slug');
+            $table->text('body');
+            $table->boolean('status');
+            $table->integer('posted_by');
+            $table->string('image');
+            $table->string('reaction')->nullable();
+            $table->string('views')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
