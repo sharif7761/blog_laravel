07d03be8267b730f11a2389321c5c6067e192cd7
Puspita Vabna <sharif7761@gmail.com>
2021-08-31 01:19:35 +0600

    post, tag and category relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class, 'post_tags');
+    }
+
+    public function categories(){
+        return $this->belongsToMany(Category::class, 'category_posts');
+    }
 }
